f42ae0dbb48508351811565dc059d49704b0ce6b
Wally869 <wally869@protonmail.com>
2025-10-03 13:16:18 +0200

    feat: add GitHub Pages deployment with static adapter

---
+++ floraison-ui/src/routes/+layout.ts
@@ -0,0 +1,5 @@
+// Enable prerendering for the entire app (required for static adapter)
+export const prerender = true;
+
+// Disable SSR since GitHub Pages serves only static files
+export const ssr = false;
